ae0d33a2c4844d0aa4f9a7d96bf3d9f5b9fd860b
Strider <strange.transistor@gmail.com>
2020-02-13 21:22:57 +0300

    builtin Boolean, Number.

--- builtin.js
+++ builtin.js
@@ -2,6 +2,26 @@
 import Contract from './Contract'
 
 
+export var Boolean = Contract('Boolean', value =>
+{
+	if (typeof value !== 'boolean')
+	{
+		return 'must_be_boolean'
+	}
+
+	return true
+})
+
+export var Number = Contract('Number', value =>
+{
+	if (typeof value !== 'number')
+	{
+		return 'must_be_number'
+	}
+
+	return true
+})
+
 export var String = Contract('String', value =>
 {
 	if (typeof value !== 'string')
@@ -15,9 +35,9 @@ export var String = Contract('String', value =>
 
 export default new Map(
 [
-	// [ global.Boolean, Boolean ],
-	// [ global.Number, Number ],
-	[ global.String, String ],
+	[ global.Boolean, Boolean ],
+	[ global.Number,  Number ],
+	[ global.String,  String ],
 
 	// [ global.Null, Null ],
 ])
